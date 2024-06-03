bd116288f4ec6735dccc9be61b23a67ddcc7e678
veronikablinn <veronika2004blinn@gmail.com>
2024-06-04 01:27:34 +0200

    first commit

--- ShoppingCart.Tests/CategoryControllerTests.cs
+++ ShoppingCart.Tests/CategoryControllerTests.cs
@@ -1,90 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Moq;
+﻿using Moq;
 using ShoppingCart.DataAccess.Repositories;
 using ShoppingCart.DataAccess.ViewModels;
 using ShoppingCart.Models;
-using System;
-using System.Collections.Generic;
+using ShoppingCart.Tests.Datasets;
+using ShoppingCart.Web.Areas.Admin.Controllers;
 using System.Linq.Expressions;
 using Xunit;
 
-namespace ShoppingCart.Web.Areas.Admin.Controllers.Tests
+namespace ShoppingCart.Tests
 {
     public class CategoryControllerTests
     {
-        // Тест 1 - Перевірка видалення категорії
         [Fact]
-        public void DeleteCategory_Check()
+        public void GetCategories_All_ReturnAllCategories()
         {
-            int categoryId = 1;
-            var category = new Category { Id = categoryId, Name = "Category 1" };
+            Mock<ICategoryRepository> repositoryMock = new Mock<ICategoryRepository>();
 
-            var repositoryMock = new Mock<ICategoryRepository>();
-            repositoryMock.Setup(r => r.GetT(It.IsAny<Expression<Func<Category, bool>>>(), null))
-                          .Returns(category);
-
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
-            unitOfWorkMock.Setup(uow => uow.Category).Returns(repositoryMock.Object);
-
-            var controller = new ShoppingCart.Web.Areas.Admin.Controllers.CategoryController(unitOfWorkMock.Object);
-
-            controller.DeleteData(categoryId);
-
-            repositoryMock.Verify(r => r.Delete(category), Times.Once);
-            unitOfWorkMock.Verify(uow => uow.Save(), Times.Once);
-        }
-
-        // Тест 2 - Перевірка виключення при створенні/оновленні з невалідною моделлю
-        [Fact]
-        public void CreateUpdateInvalidModel_Check()
-        {
-            var categoryVM = new CategoryVM { Category = new Category { Id = 0, Name = "" } };
-
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
-            var controller = new ShoppingCart.Web.Areas.Admin.Controllers.CategoryController(unitOfWorkMock.Object);
-            controller.ModelState.AddModelError("Name", "Required");
-
-            var exception = Assert.Throws<Exception>(() => controller.CreateUpdate(categoryVM));
-            Assert.Equal("Model is invalid", exception.Message);
-        }
-
-        // Тест 3 - Перевірка отримання всіх категорій
-        [Fact]
-        public void GetReturnsAllCategories_Check()
-        {
-            var expectedCategories = new List<Category> { new Category { Id = 1, Name = "Category 1" }, new Category { Id = 2, Name = "Category 2" } };
-            var repositoryMock = new Mock<ICategoryRepository>();
-            repositoryMock.Setup(r => r.GetAll(null)).Returns(expectedCategories);
-
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
-            unitOfWorkMock.Setup(uow => uow.Category).Returns(repositoryMock.Object);
-
-            var controller = new ShoppingCart.Web.Areas.Admin.Controllers.CategoryController(unitOfWorkMock.Object);
+            repositoryMock.Setup(r => r.GetAll(It.IsAny<string>()))
+                .Returns(() => CategoryDataset.Categories);
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.Setup(uow => uow.Category).Returns(repositoryMock.Object);
+            var controller = new CategoryController(mockUnitOfWork.Object);
 
             var result = controller.Get();
 
-            Assert.Equal(expectedCategories, result.Categories);
-        }
-
-        // Тест 4 - Перевірка отримання категорії за Id
-        [Fact]
-        public void GetCategoryById_CHeck()
-        {
-            int categoryId = 1;
-            var expectedCategory = new Category { Id = categoryId, Name = "Category 1" };
-
-            var repositoryMock = new Mock<ICategoryRepository>();
-            repositoryMock.Setup(r => r.GetT(It.IsAny<Expression<Func<Category, bool>>>(), null))
-                          .Returns(expectedCategory);
-
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
-            unitOfWorkMock.Setup(uow => uow.Category).Returns(repositoryMock.Object);
-
-            var controller = new ShoppingCart.Web.Areas.Admin.Controllers.CategoryController(unitOfWorkMock.Object);
-
-            var result = controller.Get(categoryId);
-
-            Assert.Equal(expectedCategory, result.Category);
+            Assert.Equal(CategoryDataset.Categories, result.Categories);
         }
     }
 }
--- ShoppingCartWeb/Controllers/OrderController.cs
+++ ShoppingCartWeb/Controllers/OrderController.cs
@@ -4,6 +4,9 @@ using ShoppingCart.DataAccess.Repositories;
 using ShoppingCart.DataAccess.ViewModels;
 using ShoppingCart.Models;
 using ShoppingCart.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using Xunit;
 
@@ -11,10 +14,27 @@ namespace ShoppingCart.Web.Areas.Admin.Controllers
 {
     public class OrderControllerTests
     {
-        
+        // Тест 1
+        [Fact]
+        public void OrderDetails_OrderVM()
+        {
+            var unit_of_work_mock = new Mock<IUnitOfWork>();
+            var order_header = new OrderHeader { Id = 1, ApplicationUser = new ApplicationUser() };
+            var order_details = new List<OrderDetail> { new OrderDetail { OrderHeaderId = 1 } };
+            unit_of_work_mock.Setup(uow => uow.OrderHeader.GetT(It.IsAny<Expression<Func<OrderHeader, bool>>>(), It.IsAny<string>())).Returns(order_header);
+            unit_of_work_mock.Setup(uow => uow.OrderDetail.GetAll(It.IsAny<string>())).Returns(order_details.AsQueryable());
+            var controller = new OrderController(unit_of_work_mock.Object);
+
+            var result = controller.OrderDetails(1);
+
+            Assert.NotNull(result);
+            Assert.Equal(order_header, result.OrderHeader);
+            Assert.Equal(order_details, result.OrderDetails.ToList());
+        }
+
         // Тест 2
         [Fact]
-        public void UpdatesOrderStatus_Check()
+        public void SetToInProcess()
         {
             var unit_of_work_mock = new Mock<IUnitOfWork>();
             var order_header = new OrderHeader { Id = 1 };
@@ -32,7 +52,7 @@ namespace ShoppingCart.Web.Areas.Admin.Controllers
 
         // Тест 3
         [Fact]
-        public void UpdatesShippedStatus_Check()
+        public void SetToShipped()
         {
             var unit_of_work_mock = new Mock<IUnitOfWork>();
             var orderHeaderRepositoryMock = new Mock<IOrderHeaderRepository>();
